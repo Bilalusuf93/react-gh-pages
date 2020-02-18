21d6e0628c06d7fe9794fedf4e81d8bfda675263
bilal <bilal@DESKTOP-LELT5N5>
2020-02-18 14:57:10 +0500

    Updates

---
+++ precache-manifest.18d7bb7caca8be1e128408c01a454da8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ccd4553c4cf5717d4f2a7eababf5044",
+    "url": "/Bilalusuf93/react-gh-pages/index.html"
+  },
+  {
+    "revision": "6e11fa6de61c1b4adaad",
+    "url": "/Bilalusuf93/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "651907a7e02234f7cfcc",
+    "url": "/Bilalusuf93/react-gh-pages/static/js/2.33af256b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Bilalusuf93/react-gh-pages/static/js/2.33af256b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e11fa6de61c1b4adaad",
+    "url": "/Bilalusuf93/react-gh-pages/static/js/main.19621b29.chunk.js"
+  },
+  {
+    "revision": "5cd9cb1ca9195fab0061",
+    "url": "/Bilalusuf93/react-gh-pages/static/js/runtime-main.1511391a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Bilalusuf93/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
